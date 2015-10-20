95997ca8c08c58cddb3bb52cf29497e974e16366
Luke Fieldsend <lukefieldsend@googlemail.com>
2015-10-20 17:06:38 +0100

    did the initial boilerPlate to handle pkPayment

---
+++ JudoPayDotNet/Models/PKPaymentModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JudoPayDotNet.Models
+{
+    [DataContract]
+    public class PKPaymentModel : PaymentModel
+    {
+        /// <summary>
+        /// Gets or sets the card number.
+        /// </summary>
+        /// <value>
+        /// The card number.
+        /// </value>
+        [DataMember(IsRequired = true)]
+        public string CardNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the expiry date.
+        /// </summary>
+        /// <value>
+        /// The expiry date.
+        /// </value>
+        [DataMember(IsRequired = true)]
+        public string ExpiryDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the start date.
+        /// </summary>
+        /// <value>
+        /// The start date.
+        /// </value>
+        [DataMember(IsRequired = false, EmitDefaultValue = false)]
+        public string StartDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Issue Number.
+        /// </summary>
+        /// <value>
+        /// The Issue Number date.
+        /// </value>
+        [DataMember(IsRequired = false)]
+        public string IssueNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the card address.
+        /// </summary>
+        /// <value>
+        /// The card address.
+        /// </value>
+        [DataMember(EmitDefaultValue = false)]
+        public CardAddressModel CardAddress { get; set; }
+    }
+}
---
+++ JudoPayDotNet/Models/Validations/PKPaymentValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+
+namespace JudoPayDotNet.Models.Validations
+{
+    internal class PKPaymentValidator: PaymentsBaseValidator<PKPaymentModel>
+	{
+        public PKPaymentValidator()
+		{
+			RuleFor(model => model.CardNumber)
+				.NotEmpty().WithMessage("You must supply your consumer token");
+
+			RuleFor(model => model.ExpiryDate)
+				.NotEmpty().WithMessage("You must supply your card token");
+		}
+	}
+}
