9aa8cd817e77dfea3ffe9e20110cef072b8d40f1
Roland82 <roland.ormrod@googlemail.com>
2016-02-01 11:31:39 +0000

    Adding internal factory method to control API version being called

--- JudoPayDotNet/Properties/AssemblyInfo.cs
+++ JudoPayDotNet/Properties/AssemblyInfo.cs
@@ -27,6 +27,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: 
+    InternalsVisibleTo("Judo.Tests.Regression"),
 	InternalsVisibleTo("JudoPayDotNetTests"), 
 	InternalsVisibleTo("JudoPayDotNetDotNet"), 
 	InternalsVisibleTo("JudoPayDotNetIntegrationTests"), 
