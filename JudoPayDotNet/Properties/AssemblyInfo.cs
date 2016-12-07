123c814bf4c0d0227ff2ec4e03ab215851801ae6
Michael Hancock <mikehan99@hotmail.com>
2016-12-07 09:10:12 +0000

    exposing internals for testing (#63)
    * Update and rename License.txt to LICENSE (#57)
    
    * Update Readme.md (#58)
    
    * ensuring headers are set for client usage (#56)
    
    * updates to User agent (#61)
    
    * pushing details to user agent
    
    * adding tests
    
    * wip
    
    * fixing up the interface for custom user agents
    
    * added another test for duplicate user agent entries
    
    * minor tweaks to the interface
    
    * exposing internals for testing

--- JudoPayDotNet/Properties/AssemblyInfo.cs
+++ JudoPayDotNet/Properties/AssemblyInfo.cs
@@ -26,8 +26,10 @@ using System.Runtime.CompilerServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: 
-	InternalsVisibleTo("JudoPayDotNetTests"), 
+[assembly:
+    InternalsVisibleTo("Judo.Tests.Base"),
+    InternalsVisibleTo("Judo.Tests.Regression"),
+    InternalsVisibleTo("JudoPayDotNetTests"), 
 	InternalsVisibleTo("JudoPayDotNetDotNet"), 
 	InternalsVisibleTo("JudoPayDotNetIntegrationTests"), 
 	InternalsVisibleTo("JudoPayDotNetWindowsPhone"),
