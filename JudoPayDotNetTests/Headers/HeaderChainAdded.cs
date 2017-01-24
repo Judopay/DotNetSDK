a1789a7aa8a611004ef56df0598ac716df36dd6b
James Goodwin <jamesgoodwin@gmail.com>
2017-01-24 17:05:07 +0000

    Fix test

--- JudoPayDotNetTests/Headers/HeaderChainAdded.cs
+++ JudoPayDotNetTests/Headers/HeaderChainAdded.cs
@@ -118,8 +118,8 @@ namespace JudoPayDotNetTests.Headers
                                   {
                                       // Ensure User-Agent is sent
                                       Assert.That(request.Headers.UserAgent, Is.Not.Null.Or.Empty);
-                                      Assert.That(request.Headers.UserAgent.Count, Is.EqualTo(4));
 
+    				      Assert.That(request.Headers.UserAgent.Count, Is.EqualTo(2));
                                       Assert.That(request.Headers.UserAgent.Any(a => a.Product.Name == "TEST"));
 
                                       return Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK));
