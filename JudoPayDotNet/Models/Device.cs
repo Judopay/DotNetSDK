aa04f4b3e0fe85d9a4a5f8fbd11649d045fc4b7f
James Lappin <james.lappin@judopayments.com>
2017-06-19 18:50:27 +0100

    Judo is now returning the device

---
+++ JudoPayDotNet/Models/Device.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace JudoPayDotNet.Models
+{
+    /// <summary>
+    /// Details of the device being used in the requested operation (add card/payment)
+    /// </summary>
+    // ReSharper disable UnusedAutoPropertyAccessor.Global
+    [DataContract(Name = "Device", Namespace = "")]
+    public class Device
+    {
+        /// <summary>
+        /// Gets or sets the consumer token.
+        /// </summary>
+        /// <value>
+        /// The consumer token.
+        /// </value>
+        [DataMember(EmitDefaultValue = false)]
+        public string Identity { get; set; }
+
+        // ReSharper restore UnusedAutoPropertyAccessor.Global
+    }
+}
