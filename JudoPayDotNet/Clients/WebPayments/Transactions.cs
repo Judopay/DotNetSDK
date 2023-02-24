d94bbb8f8224c765e23681dfd38ed91da4b48eb3
Stuart Baillie <stuart.baillie@judopay.com>
2023-02-24 12:11:03 +0000

    JR-6004 : Remove Transactions.get(type, reference) as that is not exposed through an API

--- JudoPayDotNet/Clients/WebPayments/Transactions.cs
+++ JudoPayDotNet/Clients/WebPayments/Transactions.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Threading.Tasks;
-using JudoPayDotNet.Errors;
 using JudoPayDotNet.Http;
 using JudoPayDotNet.Logging;
 using JudoPayDotNet.Models;
@@ -33,31 +32,6 @@ namespace JudoPayDotNet.Clients.WebPayments
         }
 
         /// <summary>
-        /// Gets a webpayment transaction by it's reference filtering by type
-        /// </summary>
-        /// <param name="reference">The reference.</param>
-        /// <param name="type">The type of transaction <see cref="TransactionType"/></param>
-        /// <returns>The webpayment transaction</returns>
-        public Task<IResult<GetWebPaymentResponseModel>> Get(string reference, TransactionType type)
-
-        {
-            if (type == TransactionType.PREAUTH || type == TransactionType.PAYMENT)
-            {
-                var address = $"{Baseaddress}/{type}/{reference}";
-
-                return GetInternal<GetWebPaymentResponseModel>(address);
-            }
-
-            //Transactions can only be fetched by transation types PAYMENTS or PREAUTH
-            return Task.FromResult<IResult<GetWebPaymentResponseModel>>(new Result<GetWebPaymentResponseModel>(null,
-                new ModelError()
-                {
-                    Message = "Sorry, it looks like you're trying to make a collection on an invalid transaction type. Collections can only be performed on PreAuths.",
-                    Code = 43//JudoApiError.General_Error TODO make enum to represent these code numbers
-                }));
-        }
-
-		/// <summary>
 		/// Gets a webpayment transaction by transaction identifier (ReceiptId).
 		/// </summary>
 		/// <param name="receiptId">The transaction identifier.</param>
